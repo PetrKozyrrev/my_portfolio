a819b0323bb0c82ccca1942e930d29c73c174675
Petr Kozyrev <63793503+PetrKozyrrev@users.noreply.github.com>
2022-06-25 11:24:59 +0300

    Add files via upload

---
+++ my_portfolio-main/js/main.js
@@ -0,0 +1,64 @@
+/* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex = 4;
+
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides = document.getElementsByClassName("protfolio_item");
+    
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+      slideIndex = 4
+    }
+    if (n < 4) {
+        slideIndex = slides.length
+    }
+  
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+
+    const pageWidth = document.documentElement.scrollWidth;
+    if (pageWidth>993){
+        /* Делаем элемент блочным: */
+        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 2].style.display = "block";
+        slides[slideIndex - 3].style.display = "block";
+        slides[slideIndex - 4].style.display = "block"; 
+    }
+    if (pageWidth<=993 && pageWidth>768){
+        /* Делаем элемент блочным: */
+        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 2].style.display = "block";
+        slides[slideIndex - 3].style.display = "block";
+    }
+    if (pageWidth<=768 && pageWidth>500){
+        /* Делаем элемент блочным: */
+        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 2].style.display = "block";
+    }
+    if (pageWidth<500){
+        /* Делаем элемент блочным: */
+        slides[slideIndex - 1].style.display = "block";
+    }
+    
+}
